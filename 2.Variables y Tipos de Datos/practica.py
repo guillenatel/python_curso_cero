5640f10f7a94d0198603cea997a31f809cd89611
guillenatel <guillermina.natel@alumnos.frm.utn.edu.ar>
2023-11-01 12:45:50 -0300

    Subo carpetas 2-3-4-5

---
+++ 2.Variables y Tipos de Datos/practica.py
@@ -0,0 +1,9 @@
+#Escribir fecha en formato dd/mm/aaaa
+
+año = 2023;
+mes = 10;
+dia = 23;
+
+print ("La fecha del dia de hoy es " ,dia,"/",mes,"/",año);
+print(f"La fecha del dia de hoy es {dia}/{mes}/{año}"); 
+
