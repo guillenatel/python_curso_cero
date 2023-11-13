6c9ad0b5e1ecd8aef665d41685224dea9eea69d4
guillenatel <guillermina.natel@alumnos.frm.utn.edu.ar>
2023-11-13 00:56:36 -0300

    Modifique nombre de carpetas y realice actividades de Listas

---
+++ 1.Variables y Tipos de Datos/practica.py
@@ -0,0 +1,10 @@
+#Escribir fecha en formato dd/mm/aaaa
+
+año = 2023;
+mes = 10;
+dia = 23;
+
+print ("La fecha del dia de hoy es " ,dia,"/",mes,"/",año);
+print(f"La fecha del dia de hoy es {dia}/{mes}/{año}"); 
+
+
